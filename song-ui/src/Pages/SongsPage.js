248404029d58990e51be47002e587c949fd162fe
DESKTOP-OPM29B3\G <g.safonovas@gmail.com>
2022-07-20 11:38:24 +0300

    [React frontend] render songs in table

--- song-ui/src/Pages/SongsPage.js
+++ song-ui/src/Pages/SongsPage.js
@@ -1,45 +1,45 @@
 import {Table} from "react-bootstrap";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {getSongs} from "../API/apiEndpoints";
 
 const SongsPage = () => {
 
-    useEffect(() =>{
-        getSongs().then((response)=>console.log('response', response))
-            .catch((error)=>console.log('error', error))
+    const [songs, setSongs] = useState([])
+
+    useEffect(() => {
+        getSongs().then((response) => {
+            console.log('response', response)
+            setSongs(response.data)
+
         })
+            .catch((error) => console.log('error', error))
+    }, [])
+
+    console.log('songs', songs)
 
     return (
-        <Table striped bordered hover variant="dark">
-            <thead>
-            <tr>
-                <th>#</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Username</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td>1</td>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
-            <tr>
-                <td>2</td>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-            </tr>
-            <tr>
-                <td>3</td>
-                <td colSpan={2}>Larry the Bird</td>
-                <td>@twitter</td>
-            </tr>
-            </tbody>
-        </Table>
-
+            <Table striped bordered hover variant="dark">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Song name</th>
+                    <th>Album</th>
+                    <th>Author</th>
+                    <th>Playtime</th>
+                </tr>
+                </thead>
+                <tbody>
+                {songs.map((song, i) => (
+                    <tr key={song.id}>
+                        <td>{i+1}</td>
+                        <td>{song.songName}</td>
+                        <td>{song.albumString}</td>
+                        <td>{song.authorString}</td>
+                        <td>{song.playtime}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </Table>
     )
 }
 export default SongsPage
