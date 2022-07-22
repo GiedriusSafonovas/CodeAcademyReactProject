a93b941bd9fbf97ed6d7c5e1c639cbad22c2ab14
DESKTOP-OPM29B3\G <g.safonovas@gmail.com>
2022-07-22 14:29:13 +0300

    [React frontend] redux added

--- song-ui/src/App.js
+++ song-ui/src/App.js
@@ -2,18 +2,24 @@ import SongsPage from "./Pages/SongsPage";
 import Header from "./Header/Header";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import AddSong from "./Pages/AddSong";
+import UpdateSong from "./Pages/UpdateSong";
+import {Provider} from "react-redux";
+import store from "./Redux/store";
 
 function App() {
     return (
-        <BrowserRouter>
-        <div className="App">
-            <Header/>
-            <Routes>
-                <Route path='/songlist' element={<SongsPage/>}/>
-                <Route path='/addsong' element={<AddSong/>}/>
-            </Routes>
-        </div>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <div className="App">
+                    <Header/>
+                    <Routes>
+                        <Route path='/songlist' element={<SongsPage/>}/>
+                        <Route path='/addsong' element={<AddSong/>}/>
+                        <Route path='/updatesong' element={<UpdateSong/>}/>
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </Provider>
     );
 }
 
