edc2ab3f3339517f2993732bafe0e6e27295024e
DESKTOP-OPM29B3\G <g.safonovas@gmail.com>
2022-07-24 15:44:21 +0300

    [Backend] - save a liked song to a playlist

--- src/main/java/lt/codeacademy/reactprojektas/controller/PlaylistController.java
+++ src/main/java/lt/codeacademy/reactprojektas/controller/PlaylistController.java
@@ -1,39 +1,52 @@
 package lt.codeacademy.reactprojektas.controller;
 
 import lombok.RequiredArgsConstructor;
+import lt.codeacademy.reactprojektas.dto.PlaylistRequest;
 import lt.codeacademy.reactprojektas.service.PlaylistService;
+import org.springframework.security.web.header.Header;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class PlaylistController {
     private final PlaylistService playlistService;
 
-    @PostMapping("/LikeSong/{songId}/{page}/{sort}")
-    public String likeSong(Principal principal, @PathVariable Long songId, @PathVariable String page, @PathVariable String sort){
-        Long playlistId = playlistService.getLikedSongPlaylistId(principal.getName());
-        playlistService.addSongToPlaylist(songId, playlistId);
-        return "redirect:/songs" + "?page=" + page + "&sort=" + sort;
+    @PostMapping("/likesong")
+    public void likeSong(@RequestBody PlaylistRequest playlistRequest){
+
+        System.out.println(playlistRequest.getUsername());
+        Long playlistId = playlistService.getLikedSongPlaylistId(playlistRequest.getUsername());
+        playlistService.addSongToPlaylist(playlistRequest.getSongId(), playlistId);
     }
 
-    @PostMapping("/UnLikeSong/{songId}/{page}/{sort}")
-    public String unLikeSong(Principal principal, @PathVariable Long songId, @PathVariable String page, @PathVariable String sort){
-
-        Long playlistId = playlistService.getLikedSongPlaylistId(principal.getName());
-        playlistService.removeSongFromPlaylist(songId, playlistId);
-        return "redirect:/songs" + "?page=" + page + "&sort=" + sort;
-    }
-
-    @PostMapping("/UnLikeSong/{songId}")
-    public String unLikeSong(Principal principal, @PathVariable Long songId){
-
-        Long playlistId = playlistService.getLikedSongPlaylistId(principal.getName());
-        playlistService.removeSongFromPlaylist(songId, playlistId);
-        return "redirect:/";
-    }
+//    @PostMapping("/LikeSong/{songId}/{page}/{sort}")
+//    public String likeSong(Principal principal, @PathVariable Long songId, @PathVariable String page, @PathVariable String sort){
+//        Long playlistId = playlistService.getLikedSongPlaylistId(principal.getName());
+//        playlistService.addSongToPlaylist(songId, playlistId);
+//        return "redirect:/songs" + "?page=" + page + "&sort=" + sort;
+//    }
+//
+//    @PostMapping("/UnLikeSong/{songId}/{page}/{sort}")
+//    public String unLikeSong(Principal principal, @PathVariable Long songId, @PathVariable String page, @PathVariable String sort){
+//
+//        Long playlistId = playlistService.getLikedSongPlaylistId(principal.getName());
+//        playlistService.removeSongFromPlaylist(songId, playlistId);
+//        return "redirect:/songs" + "?page=" + page + "&sort=" + sort;
+//    }
+//
+//    @PostMapping("/UnLikeSong/{songId}")
+//    public String unLikeSong(Principal principal, @PathVariable Long songId){
+//
+//        Long playlistId = playlistService.getLikedSongPlaylistId(principal.getName());
+//        playlistService.removeSongFromPlaylist(songId, playlistId);
+//        return "redirect:/";
+//    }
 
 }
---
+++ src/main/java/lt/codeacademy/reactprojektas/dto/PlaylistRequest.java
@@ -0,0 +1,11 @@
+package lt.codeacademy.reactprojektas.dto;
+
+import lombok.Data;
+
+@Data
+public class PlaylistRequest {
+
+    private String username;
+    private Long songId;
+
+}
