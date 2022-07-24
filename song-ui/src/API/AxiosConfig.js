320d4079bfb5d0ca957bf90fdc3d19a6f3186741
DESKTOP-OPM29B3\G <g.safonovas@gmail.com>
2022-07-24 13:42:56 +0300

    [React frontend] added axios interceptor for user authorization

--- song-ui/src/API/AxiosConfig.js
+++ song-ui/src/API/AxiosConfig.js
@@ -1,7 +1,20 @@
 import axios from "axios";
+import {loadFromLocalStorage} from "../Storage/localStorage";
 
 const HTTP = axios.create({
     baseURL: ''
+})
+
+HTTP.interceptors.request.use(config => {
+    const user = loadFromLocalStorage('user')
+
+    // console.log("Before every request jwt", user)
+
+    if (user) {
+        config.headers.authorization = "Bearer " + user.jwtToken
+    }
+
+    return config
 })
 
 export default HTTP
--- src/main/java/lt/codeacademy/reactprojektas/security/JwtSecurityConfig.java
+++ src/main/java/lt/codeacademy/reactprojektas/security/JwtSecurityConfig.java
@@ -36,7 +36,7 @@ public class JwtSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/login", "/songs", "/createsong", "/deletesong/**", "/updatesong").permitAll()
+                .antMatchers("/login", "/songs").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
