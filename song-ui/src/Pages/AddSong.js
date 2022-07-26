693e2f3ee7ae3c40013277105534cb9f4cf4d71f
DESKTOP-OPM29B3\G <g.safonovas@gmail.com>
2022-07-26 14:25:48 +0300

    [React frontend] add formik and validation to add song page

--- song-ui/src/Pages/AddSong.js
+++ song-ui/src/Pages/AddSong.js
@@ -1,81 +1,154 @@
-import {Form} from "react-bootstrap";
+import {Form as BootstrapForm} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import {useState} from "react";
-import {createSong} from "../API/apiEndpoints";
+import {createSong, getLikedSongs} from "../API/apiEndpoints";
 import {useSelector} from "react-redux";
 import {useTranslation} from "react-i18next";
+import {ErrorMessage, Field, Formik, Form} from "formik";
+import {useState} from "react";
+import {saveToLocalStorage} from "../Storage/localStorage";
+import {setLikedSongsinReducer, setUserData} from "../Redux/Actions";
 
-const AddSong = () =>
-{
+
+const AddSong = () => {
+
+    const [result, setResult] = useState(null)
+
     const user = useSelector(state => state.user)
 
-    const { t } = useTranslation("songTable")
+    const {t} = useTranslation("songTable")
 
-    const [song, setSong] = useState({
-        songName: '',
-        albumString: '',
-        authorString: '',
-        playtime: '',
-    })
 
-    const handleChange = (e) => {
-        setSong({
-            ...song,
-            [e.target.name]: e.target.value,
-        })
+    const validate = (song) => {
+        const errors = {}
+
+        if (song.songName.length < 1) {
+            errors.songName = t("errors:blank")
+        }
+
+        if (song.albumString.length < 1) {
+            errors.albumString = t("errors:blank")
+        }
+
+        if (song.authorString.length < 1) {
+            errors.authorString = t("errors:blank")
+        }
+
+        if (song.playtime.length < 1) {
+            errors.playtime = t("errors:blank")
+        }
+        return errors
     }
 
-    const onSubmit = (e) =>{
-        e.preventDefault()
-        createSong(song)
+    const handleResult = (result) => {
+        setResult(result)
     }
 
 
-    return(<>
-        {user.roles.includes("ROLE_ADMIN") &&
-        <Form onSubmit={onSubmit}>
-            <Form.Group className="mb-3" controlId="name">
-                <Form.Label>{t("songName")}</Form.Label>
-                <Form.Control type="text"
-                              placeholder={t("placeholders:songName")}
-                              name = 'songName'
-                              onChange={handleChange}
-                />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="album">
-                <Form.Label>{t("album")}</Form.Label>
-                <Form.Control type="text"
-                              placeholder={t("placeholders:album")}
-                              name = 'albumString'
-                              onChange={handleChange}
-                />
-                <Form.Text className="text-muted">
-                    Enter multiple with ;
-                </Form.Text>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="author">
-                <Form.Label>{t("author")}</Form.Label>
-                <Form.Control type="text"
-                              placeholder={t("placeholders:author")}
-                              name = 'authorString'
-                              onChange={handleChange}
-                />
-                <Form.Text className="text-muted">
-                    Enter multiple with ;
-                </Form.Text>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="playtime">
-                <Form.Label>{t("playtime")}</Form.Label>
-                <Form.Control type="text"
-                              placeholder={t("placeholders:playtime")}
-                              name = 'playtime'
-                              onChange={handleChange}
-                />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                {t("common:submit")}
-            </Button>
-        </Form>}
+    return (<>
+            {user.roles.includes("ROLE_ADMIN") &&
+                <Formik initialValues={{
+                    songName: '',
+                    albumString: '',
+                    authorString: '',
+                    playtime: '',
+                }}
+                        onSubmit={(song, helper) => {
+                            createSong(song).then(()=>{
+                                handleResult("success")
+                            }).catch(()=>{
+                                handleResult("error")
+                            })
+                        }}
+                        validate={validate}>
+                    {props => {
+                        return (
+                            <Form>
+                                <ErrorMessage name='songName'/>
+                                <Field name='songName'>
+                                    {({field, form}) => {
+                                        return (
+                                            <BootstrapForm.Group className="mb-3" controlId="name">
+                                                <BootstrapForm.Label>{t("songName")}</BootstrapForm.Label>
+                                                <BootstrapForm.Control type="text"
+                                                                       placeholder={t("placeholders:songName")}
+                                                                       name='songName'
+                                                                       onChange={field.onChange}
+
+                                                />
+                                            </BootstrapForm.Group>
+                                        )
+                                    }}
+                                </Field>
+
+                                <ErrorMessage name='albumString'/>
+                                <Field name='albumString'>
+                                    {({field, form}) => {
+                                        return (
+                                            <BootstrapForm.Group className="mb-3" controlId="album">
+                                                <BootstrapForm.Label>{t("album")}</BootstrapForm.Label>
+                                                <BootstrapForm.Control type="text"
+                                                                       placeholder={t("placeholders:album")}
+                                                                       name='albumString'
+                                                                       onChange={field.onChange}
+
+                                                />
+                                                <BootstrapForm.Text className="text-muted">
+                                                    Enter multiple with ;
+                                                </BootstrapForm.Text>
+                                            </BootstrapForm.Group>
+                                        )
+                                    }}
+                                </Field>
+
+                                <ErrorMessage name='authorString'/>
+                                <Field name='authorString'>
+                                    {({field, form}) => {
+                                        return (
+                                            <BootstrapForm.Group className="mb-3" controlId="author">
+                                                <BootstrapForm.Label>{t("author")}</BootstrapForm.Label>
+                                                <BootstrapForm.Control type="text"
+                                                                       placeholder={t("placeholders:author")}
+                                                                       name='authorString'
+                                                                       onChange={field.onChange}
+
+                                                />
+                                                <BootstrapForm.Text className="text-muted">
+                                                    Enter multiple with ;
+                                                </BootstrapForm.Text>
+                                            </BootstrapForm.Group>
+                                        )
+                                    }}
+                                </Field>
+
+                                <ErrorMessage name='playtime'/>
+                                <Field name='playtime'>
+                                    {({field, form}) => {
+                                        return (
+                                            <BootstrapForm.Group className="mb-3" controlId="playtime">
+                                                <BootstrapForm.Label>{t("playtime")}</BootstrapForm.Label>
+                                                <BootstrapForm.Control type="text"
+                                                                       placeholder={t("placeholders:playtime")}
+                                                                       name='playtime'
+                                                                       onChange={field.onChange}
+
+                                                />
+                                            </BootstrapForm.Group>
+                                        )
+                                    }}
+                                </Field>
+                                <Button variant="primary" type="submit">
+                                    {t("common:submit")}
+                                </Button>
+
+
+                            </Form>)
+                    }}
+                </Formik>
+
+            }
+            {result &&
+                <span>{t("common:"+result)}</span>
+            }
         </>
     )
 }
